1ae68f74abc665c2ba151fdb4f14b18522a97a4b
BabyBeo2k1 <hung.lq191881@sis.hust.edu.vn>
2023-07-10 13:58:08 +0700

    first version cfg

--- Hung/stmtCFG.py
+++ Hung/stmtCFG.py
@@ -1,12 +1,12 @@
 import javalang
 import networkx as nx
-def check_pred(cfg:nx.DiGraph,source,type:type):
+import copy
+def check_pred(cfg:nx.DiGraph,source,type):
     for pred in cfg.predecessors(source):
         if isinstance(pred,type):
             return pred
         else:
             return check_pred(cfg,pred,type)
-    
 def buildNode(cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.ast.Node):
     #interface of build a stmt
     cfgBuilder= stmtCFG()
@@ -16,30 +16,51 @@ def buildNode(cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.ast.Node
 class stmtCFG:
     def buildNode(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.ast.Node):
         #abstract method to build next node
-        
-        if isinstance(node,javalang.tree.IfStatement):
+        if isinstance(node,type(None)):
+           return prev,cfg
+        elif isinstance(node,javalang.tree.IfStatement):
             return self.buildIf(cfg,prev,node)
         elif isinstance(node,javalang.tree.ForStatement):
             return self.buildFor(cfg,prev,node)
-        # elif isinstance(node,javalang.tree.WhileStatement):
-        #     return self.buildWhile(cfg,prev,node)
-        # elif isinstance(node,javalang.tree.DoStatement):
-        #     return self.buildDo(cfg,prev,node)
-        # elif isinstance(node,javalang.tree.BreakStatement):
-        #     return self.buiildBreak(cfg,prev,node)
-        # elif isinstance(node,javalang.tree.ContinueStatement):
-        #     return self.buildContinue(cfg,prev,node)
+        elif isinstance(node,javalang.tree.WhileStatement):
+            return self.buildWhile(cfg,prev,node)
+        elif isinstance(node,javalang.tree.DoStatement):
+            return self.buildDo(cfg,prev,node)
         elif isinstance(node,javalang.tree.ReturnStatement):
             return self.buildReturn(cfg,prev,node)
-        # elif isinstance(node,javalang.tree.TryStatement):
-        #     return self.buildTry(cfg,prev,node)
+        elif isinstance(node,javalang.tree.TryStatement):
+            return self.buildTry(cfg,prev,node)
+        elif isinstance(node,javalang.tree.SwitchStatement):
+            return self.buildSwitch(cfg,prev,node)
         elif isinstance(node,javalang.tree.BlockStatement):
             return self.buildBlock(cfg,prev,node)
+        
         else:
             return self.buildStmt(cfg,prev,node)
+    
     def buildBlock(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.BlockStatement):
         for stmt in node.statements:
-            prev,cfg=self.buildNode(cfg,prev,stmt)
+            if isinstance(stmt,(javalang.tree.BreakStatement)):
+                cfg.add_node(stmt)
+                for pre in prev:
+                    cfg.add_edge(pre,node)
+                label=check_pred(cfg,node,(javalang.tree.ForStatement,javalang.tree.DoStatement,javalang.tree.WhileStatement))
+                cfg.add_edge(stmt,label)
+                return [],cfg  
+            elif isinstance(stmt,javalang.tree.ContinueStatement):
+                cfg.add_node(stmt)
+                for pre in prev:
+                    cfg.add_edge(pre,node)
+                label=check_pred(cfg,node,(javalang.tree.ForStatement,javalang.tree.DoStatement,javalang.tree.WhileStatement))
+                if isinstance(label,javalang.tree.ForStatement):
+                    cfg.add_edge(node,label.control)
+                    label=label.control
+                else:
+                    cfg.add_edge(node,label.condition)
+                    label=label.condition
+                return [],cfg            
+            else:
+                prev,cfg=self.buildNode(cfg,prev,stmt)
         return prev,cfg
     def buildIf(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.IfStatement):
         cfg.add_node(node)
@@ -53,7 +74,7 @@ class stmtCFG:
         last_then=[node.condition]
         last_else=[node.condition]
         last_else,cfg=self.buildNode(cfg,last_else,node.else_statement)
-        last_then,cfg=self.buildNode(cfg,last_else,node.then_statement)
+        last_then,cfg=self.buildNode(cfg,last_then,node.then_statement)
         res=[]
         for i in last_else:
             res.append(i)
@@ -65,37 +86,49 @@ class stmtCFG:
         cfg.add_node(node)
         for pre in prev:
             cfg.add_edge(pre,node)
-        prev=[node]
         control=node.control
-        cfg.add_node(control.init)
-        cfg.add_node(control.condition)
+        condition=control.condition
+        init=control.init
+        update=control.update
+        prev=[node]
+        prev,cfg=self.buildNode(cfg,prev,init)
 
-        for pre in prev:
-            cfg.add_edge(pre,control.init)
-        cfg.add_edge(control.init,control.condition)
-        prev=[control.condition]
+        prev,cfg=self.buildNode(cfg,prev,condition)
+
         prev,cfg=self.buildNode(cfg,prev,node.body)
-        for update in control.update:
-            prev,cfg=self.buildNode(cfg,prev,update)
-        prev,cfg=self.buildNode(cfg,prev,control.condition)
-        prev=[control.condition]
+        for up in update:
+            prev,cfg=self.buildNode(cfg,prev,up)
+        prev,cfg=self.buildNode(cfg,prev,condition)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev=[node]
         return prev,cfg
     def buildWhile(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.WhileStatement):
-        return [],cfg
-    def buildDo(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.DoStatement):
-        return [],cfg
-    def buildBreak(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.BreakStatement):
-        return [],cfg
-    def buildContinue(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.ContinueStatement):
         cfg.add_node(node)
-        label=None
-        if node.goto==None:
-            label=check_pred(cfg,node,javalang.tree.ForStatement)
-            
-            cfg.add_edge(node,)
-        else:
-            label=node.goto
-        return [label],cfg
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev=[node]
+        prev,cfg=self.buildNode(cfg,prev,node.condition)
+        prev,cfg=self.buildNode(cfg,prev,node.body)
+        prev,cfg=self.buildNode(cfg,prev,node.condition)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev=[node]
+        return prev,cfg
+    def buildDo(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.DoStatement):
+        cfg.add_node(node)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev=[node]
+        cfg.add_node(node.control)
+        prev,cfg=self.buildNode(cfg,prev,node.body)
+        prev,cfg=self.buildNode(cfg,prev,node.condition)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev,cfg=self.buildNode(cfg,prev,node.body)
+        prev=[node]
+        return prev,cfg
+
     def buildReturn(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.ReturnStatement):
         cfg.add_node(node)
         
@@ -103,9 +136,52 @@ class stmtCFG:
             cfg.add_edge(pre,node)
         return [],cfg
     def buildTry(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.TryStatement):
-        return [],cfg
-    def buildStmt(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.Node):
+        cfg.add_node(node)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        prev=[node]
+        
+        if node.resources is not None:
+            cfg.add_nodes_from(node.resources)
+            for resource in node.resources:
+                cfg.add_edge(node,resource)
+            prev=node.resources    
+        prev_try=copy.deepcopy(prev)
+
+        for blk in node.block:
+            prev_try,cfg=self.buildNode(cfg,prev_try,blk)
+        
+        prev_catches=[]
+        for catch in node.catches:
+            prev_catch,cfg=self.buildNode(cfg,prev,catch.parameter)
+            for blk in catch.block:
+                prev_catch,cfg=self.buildNode(cfg,prev_catch,blk)
+            prev_catches.append(prev_catch)
+
+        prev=[]
+        for pre in prev_catches:
+            prev.append(pre)
+        for pre in prev_try:
+            prev.append(pre)
+        if node.finally_block is None:
+            return prev,cfg
+        else:
+            prev,cfg=self.buildNode(cfg,prev,node.finally_block)
+            return prev,cfg
+    def buildStmt(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.ast.Node):
         cfg.add_node(node)
         for pre in prev:
             cfg.add_edge(pre,node)
         return [node],cfg
+    def buildSwitch(self,cfg:nx.DiGraph,prev:list[javalang.ast.Node],node:javalang.tree.SwitchStatement):
+        cfg.add_node(node)
+        for pre in prev:
+            cfg.add_edge(pre,node)
+        cfg.add_node(node.expression)
+        cfg.add_edge(node,node.expression)
+        prev=[]
+        for case in node.cases:
+            cfg.add_node(case)
+            cfg.add_edge(node.expression)
+            prev.append(case)
+        return prev,cfg
