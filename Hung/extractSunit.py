7f85bb16affe045866bd865602c21e2a3b1ad1b0
BabyBeo2k1 <hung.lq191881@sis.hust.edu.vn>
2023-07-25 23:08:03 +0700

    add read me tututorial

--- Hung/extractSunit.py
+++ Hung/extractSunit.py
@@ -1,16 +1,16 @@
 from transformers import GPT2Tokenizer, GPT2Model
 import json 
 from methodCFG import Sunit
-class extractSunit:
-    def extract(self,inputFile,outputFile): 
-        try:
-            with open(inputFile, 'r') as in_file:
-                input='\n'.join(in_file)
-            sunit=Sunit(input)
-            sunit.composeSunit()
-            with open(outputFile,'w') as out_file:
-                out_file.write(sunit.source_code)
-        except Exception as e:
-            print(f"Error: {e}, {type(e)}")
-        
-        pass
+
+def extract(inputFile,outputFile): 
+    try:
+        with open(inputFile, 'r') as in_file:
+            input='\n'.join(in_file)
+        sunit=Sunit(input)
+        sunit.composeSunit()
+        with open(outputFile,'w') as out_file:
+            out_file.write(sunit.source_code)
+    except Exception as e:
+        print(f"Error: {e}, {type(e)}")
+    
+    pass
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Get-Ending-S_units
+
+To initiate, get requirement in conda:
+
+```
+pip install -r requirement.txt
+```python
+
+To generate sunit extraction from java files to java file, input file must not contain class or packages, only 1 method. For quick start:
+
+```
+import Hung.extractSunit
+input="input_java_file.java"
+output="output_java_file.java"
+extract(input,output)
+```python
+
+For generate sunit extraction from text to text:
+
+```
+from methodCFG import Sunit
+java_method="your java method" #your java method
+sunit=Sunit(java_method)
+sunit.composeSunit()
+print(sunit.source_code)
+
+```python
